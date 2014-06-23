8a577f2518e7b94cba0d093a5b6157e46b904e13
Jerry Clinesmith <jerryclinesmith@gmail.com>
2014-06-23 08:34:04 -0500

    Add conversion package

---
+++ convert/conversion.go
@@ -0,0 +1,169 @@
+package convert
+
+import (
+	"fmt"
+	"strconv"
+	"time"
+)
+
+var TimeFormats = []string{
+	"2006-01-02",
+}
+
+func ToString(object interface{}) string {
+	if object == nil {
+		return ""
+	}
+	return fmt.Sprintf("%v", object)
+}
+
+func ToBool(object interface{}) bool {
+	switch val := object.(type) {
+	case bool:
+		return val
+
+	case string:
+		boolVal, _ := strconv.ParseBool(val)
+		return boolVal
+
+	case uint, uint8, uint16, uint32, uint64, int, int8, int16, int32, int64:
+		return val != 0
+
+	case float32, float64:
+		return val != 0.0
+
+	default:
+		return false
+	}
+}
+
+func ToFloat(object interface{}) float64 {
+	switch val := object.(type) {
+
+	case string:
+		floatVal, _ := strconv.ParseFloat(val, 64)
+		return floatVal
+
+	case float64:
+		return val
+
+	case float32:
+		return float64(val)
+
+	case uint:
+		return float64(val)
+
+	case uint8:
+		return float64(val)
+
+	case uint16:
+		return float64(val)
+
+	case uint32:
+		return float64(val)
+
+	case uint64:
+		return float64(val)
+
+	case int:
+		return float64(val)
+
+	case int8:
+		return float64(val)
+
+	case int16:
+		return float64(val)
+
+	case int32:
+		return float64(val)
+
+	case int64:
+		return float64(val)
+
+	case bool:
+		if val {
+			return 1.0
+		} else {
+			return 0.0
+		}
+
+	default:
+		return 0.0
+	}
+}
+
+func ToInt(object interface{}) int64 {
+	switch val := object.(type) {
+
+	case string:
+		floatVal, _ := strconv.ParseFloat(val, 64)
+		return int64(floatVal)
+
+	case float64:
+		return int64(val)
+
+	case float32:
+		return int64(val)
+
+	case uint:
+		return int64(val)
+
+	case uint8:
+		return int64(val)
+
+	case uint16:
+		return int64(val)
+
+	case uint32:
+		return int64(val)
+
+	case uint64:
+		return int64(val)
+
+	case int:
+		return int64(val)
+
+	case int8:
+		return int64(val)
+
+	case int16:
+		return int64(val)
+
+	case int32:
+		return int64(val)
+
+	case int64:
+		return val
+
+	case bool:
+		if val {
+			return 1
+		} else {
+			return 0
+		}
+
+	default:
+		return 0
+	}
+}
+
+func ToTime(object interface{}) time.Time {
+	switch val := object.(type) {
+
+	case time.Time:
+		return val
+
+	case string:
+		for _, format := range TimeFormats {
+			stringVal, err := time.Parse(format, val)
+			if err == nil {
+				return stringVal
+			}
+		}
+		// Nothing worked, return Zero time
+		return time.Time{}
+
+	default:
+		return time.Time{}
+	}
+}
---
+++ convert/conversion_test.go
@@ -0,0 +1,159 @@
+package convert_test
+
+import (
+	"fmt"
+	"time"
+
+	. "github.com/koanhealth/gotools/convert"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("Conversion", func() {
+
+	Context("String", func() {
+
+		var (
+			now      = time.Now()
+			testData = map[interface{}]string{
+				nil:         "",
+				1:           "1",
+				23.6789:     "23.6789",
+				true:        "true",
+				false:       "false",
+				"My String": "My String",
+				now:         now.String(),
+			}
+		)
+
+		It("Converts correctly", func() {
+			for val, expectedVal := range testData {
+				convertedVal := ToString(val)
+				fmt.Fprintf(GinkgoWriter, "\n%T Value:%v Converted: %vExpected:%v", val, val, convertedVal, expectedVal)
+				Expect(convertedVal).To(Equal(expectedVal))
+			}
+		})
+
+	})
+
+	Context("Boolean", func() {
+
+		var (
+			now      = time.Now()
+			testData = map[interface{}]bool{
+				nil:         false,
+				uint64(1):   true,
+				0:           false,
+				23.6789:     true,
+				0.0:         false,
+				true:        true,
+				false:       false,
+				"My String": false,
+				"true":      true,
+				"1":         true,
+				"T":         true,
+				"false":     false,
+				"F":         false,
+				"0":         false,
+				now:         false,
+			}
+		)
+
+		It("Converts correctly", func() {
+			for val, expectedVal := range testData {
+				convertedVal := ToBool(val)
+				fmt.Fprintf(GinkgoWriter, "\n%T Value:%v Converted: %vExpected:%v", val, val, convertedVal, expectedVal)
+				Expect(convertedVal).To(Equal(expectedVal))
+			}
+		})
+
+	})
+
+	Context("Float", func() {
+
+		var (
+			now      = time.Now()
+			testData = map[interface{}]float64{
+				nil:         0.0,
+				uint32(5):   5.0,
+				0:           0.0,
+				23.6789:     23.6789,
+				0.0:         0.0,
+				true:        1.0,
+				false:       0.0,
+				"My String": 0.0,
+				"11.2345":   11.2345,
+				"-1":        -1.0,
+				now:         0.0,
+			}
+		)
+
+		It("Converts correctly", func() {
+			for val, expectedVal := range testData {
+				convertedVal := ToFloat(val)
+				fmt.Fprintf(GinkgoWriter, "\n%T Value:%v Converted: %vExpected:%v", val, val, convertedVal, expectedVal)
+				Expect(convertedVal).To(Equal(expectedVal))
+			}
+		})
+
+	})
+
+	Context("Integer", func() {
+
+		var (
+			now      = time.Now()
+			testData = map[interface{}]int64{
+				nil:         0,
+				uint32(5):   5,
+				0:           0,
+				23.6789:     23,
+				0.0:         0,
+				true:        1,
+				false:       0,
+				"My String": 0,
+				"11":        11,
+				"12.2345":   12,
+				"-567":      -567,
+				now:         0,
+			}
+		)
+
+		It("Converts correctly", func() {
+			for val, expectedVal := range testData {
+				convertedVal := ToInt(val)
+				fmt.Fprintf(GinkgoWriter, "\n%T Value:%v Converted: %vExpected:%v", val, val, convertedVal, expectedVal)
+				Expect(convertedVal).To(Equal(expectedVal))
+			}
+		})
+
+	})
+
+	Context("Time", func() {
+
+		var (
+			zeroTime = time.Time{}
+			now      = time.Now()
+			testData = map[interface{}]time.Time{
+				now:              now,
+				"2012-06-15":     time.Date(2012, time.June, 15, 0, 0, 0, 0, time.UTC),
+				nil:              zeroTime,
+				0:                zeroTime,
+				1.5:              zeroTime,
+				"Garbage String": zeroTime,
+				false:            zeroTime,
+				true:             zeroTime,
+			}
+		)
+
+		It("Converts correctly", func() {
+			for val, expectedVal := range testData {
+				convertedVal := ToTime(val)
+				fmt.Fprintf(GinkgoWriter, "\n%T Value:%v Converted: %vExpected:%v", val, val, convertedVal, expectedVal)
+				Expect(convertedVal).To(Equal(expectedVal))
+			}
+		})
+
+	})
+
+})
