4b3362943c1ad47944e041ed9470e5e892218fb3
Jerry Clinesmith <jerryclinesmith@gmail.com>
2018-04-05 10:11:21 -0500

    Add FormatDate

--- time/date.go
+++ time/date.go
@@ -6,6 +6,10 @@ func Date(y int, m time.Month, d int) time.Time {
 	return time.Date(y, m, d, 0, 0, 0, 0, time.UTC)
 }
 
+func FormatDate(t time.Time) string {
+	return t.Format(DateFormat)
+}
+
 const (
 	DateFormat = "1/2/2006"
 )
